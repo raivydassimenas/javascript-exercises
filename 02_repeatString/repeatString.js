2343a546596e070eca6ded8d3d25b29c72bb416b
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-09-27 14:57:31 +0300

    Do 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    outp = '';
+    for (let i = 0; i < num; i++) {
+        outp += string;
+    }
+    return outp;
 };
 
 // Do not edit below this line
