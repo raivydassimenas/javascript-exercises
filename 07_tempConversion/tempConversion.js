08d9e45bbe5712dfbcdfb5a8f94905a203ef785d
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-09-28 20:30:08 +0300

    Finish 07_tempComversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,18 @@
-const ftoc = function() {
+const ftoc = function(f) {
+  if (isNaN(f)) {
+    return "ERROR";
+  }
+
+  return Math.round(10 * ((f - 32) * 5.0/9)) / 10;
 
 };
 
-const ctof = function() {
+const ctof = function(c) {
+  if (isNaN(c)) {
+    return 'ERROR';
+  }
 
+  return Math.round(10 * (c * 9.0 / 5 + 32)) / 10;
 };
 
 // Do not edit below this line
