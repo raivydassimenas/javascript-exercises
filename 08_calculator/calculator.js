f14d2a605d3f7a5f1f9ee1014cafe9b760a3a0fc
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-09-29 16:14:49 +0300

    Finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,71 @@
-const add = function() {
-	
+const add = function(a, b) {
+	if (isNaN(a) || isNaN(b)) {
+    return 'ERROR';
+  }
+
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	if (isNaN(a) || isNaN(b)) {
+    return 'ERROR';
+  }
+
+  return a - b;
 };
 
-const sum = function() {
+const sum = function(arr) {
+  for (let num of arr) {
+    if (isNaN(num)) {
+      return 'ERROR';
+    }
+  }
 	
+  suma = 0;
+
+  for (let num of arr) {
+    suma += num;
+  }
+
+  return suma;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  for (let num of arr) {
+    if (isNaN(num)) {
+      return 'ERROR';
+    }
+  }
 
+  let prod = 1;
+
+  for (let num of arr) {
+    prod *= num;
+  }
+
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	if (isNaN(a) || isNaN(b) || a < 0) {
+    return 'ERROR';
+  }
+
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (!Number.isInteger(n) || n < 0) {
+    return 'ERROR';
+  }
+
+  fact = 1;
+
+  for (let i = 1; i <= n; i++) {
+    fact *= i;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
