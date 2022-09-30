647b0d2cf4bfa4bedb6dad115111904423ad08e7
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-09-30 19:29:58 +0300

    Finish 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    string = string.replace(/\s/g,"");
+    string = string.toUpperCase();
 
+    for (let i = 0; i < string.length/2; i++) {
+        if (string.charAt(i) !== string.charAt(string.length - 1 - i)) {
+            return false;
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
