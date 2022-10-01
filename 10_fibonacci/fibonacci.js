19b2edaadcc9a4451a1e9527052ebe1745800c6e
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-10-01 21:22:45 +0300

    Finish 09_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (typeof n === "string" && !(n = parseInt(n))) {
+        return 'OOPS';
+    }
+
+    if (!Number.isInteger(n) || n < 0) {
+        return 'OOPS';
+    }
+
+    fib1 = 1;
+    fib2 = 1;
+
+    for (let i = 3; i <= n; i++) {
+        const temp = fib1;
+        fib1 = fib2;
+        fib2 = temp + fib2;
+    }
+
+    return fib2;
 
 };
 
