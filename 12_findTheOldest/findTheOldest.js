605fca2792f54c083879b7838091fd666ddc116f
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-10-02 19:15:06 +0300

    Finish 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let person = people[0];
+    let age;
+    if (person.yearOfDeath) {
+        age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        age = new Date().getFullYear() - person.yearOfBirth;
+    }
+
+    for (let i = 1; i < people.length; i++) {
+        let currAge;
+        if (people[i].yearOfDeath) {
+            currAge = people[i].yearOfDeath - people[i].yearOfBirth;
+        } else {
+            currAge = new Date().getFullYear() - people[i].yearOfBirth;
+        }
+
+        if (age < currAge) {
+            person = people[i];
+            age = currAge;
+        }
+    }
+
+    return person;
 
 };
 
