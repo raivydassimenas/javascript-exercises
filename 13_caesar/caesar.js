4c29e7e6c8e951b9dcae6934658cf852e0e51ee0
Raivydas Šimėnas <raivydas.simenas@gmail.com>
2022-10-04 18:34:44 +0300

    13_caesar

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,27 @@
-const caesar = function() {
+const caesar = function(string, shift) {
+    let encodedString = "";
 
+    for (let ch of string) {
+        let encCh;
+        if ("A".charCodeAt(0) <= ch.charCodeAt(0) && "Z".charCodeAt(0) >= ch.charCodeAt(0)) {
+            let code = (ch.charCodeAt(0) - "A".charCodeAt(0)+ shift) % 26
+            while (code < 0) {
+                code += 26;
+            }
+            encCh = String.fromCharCode(code + "A".charCodeAt(0));
+        } else if ("a".charCodeAt(0) <= ch.charCodeAt(0) && "z".charCodeAt(0) >= ch.charCodeAt(0)) {
+            let code = (ch.charCodeAt(0) - "a".charCodeAt(0)+ shift) % 26
+            while (code < 0) {
+                code += 26;
+            }
+            encCh = String.fromCharCode(code + "a".charCodeAt(0));
+        } else {
+            encCh = ch;
+        }
+        encodedString += encCh;
+    }
+
+    return encodedString;
 };
 
 // Do not edit below this line
